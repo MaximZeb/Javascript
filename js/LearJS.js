b119e18d858f8728be8e11d532210dfd3a0a0a5e
Maxim Zebzeev <maksimzeb@yandex.ru>
2020-12-27 00:34:21 +0500

    added

--- js/LearJS.js
+++ js/LearJS.js
@@ -132,6 +132,32 @@ function setlang() {
 }
 }
 
+function define () {
+	let obj = {counter : 0};
+	Object.defineProperty(obj, "reset",{
+		get : function () {this.counter = 0;}
+	});
+	Object.defineProperty(obj, "increment", {
+  get : function () {this.counter++;}
+});
+Object.defineProperty(obj, "decrement", {
+  get : function () {this.counter--;}
+});
+Object.defineProperty(obj, "add", {
+  set : function (value) {this.counter += value;}
+});
+Object.defineProperty(obj, "subtract", {
+  set : function (value) {this.counter -= value;}
+});
+
+// Play with counter:
+obj.reset;
+obj.add = 5;
+obj.subtract = 1;
+obj.increment;
+obj.decrement;
+document.getElementById("object").innerHTML = obj.subtract = 1;
+}
 									//STRING
 function cikleForOf() {
 		var txt = "JavaScript";
