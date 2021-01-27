c08b818cebf9fcbca5c85b67df491a01acd3b194
Maxim Zebzeev <maksimzeb@yandex.ru>
2021-01-27 22:57:36 +0500

    addded class obj get set

--- js/LearJS.js
+++ js/LearJS.js
@@ -558,6 +558,7 @@ document.getElementById('addEv').addEventListener('click', function(){alert('Hel
 
 // AJAX
 // json фейковый 
+let ajax = () => {
 const requesURL = 'https://jsonplaceholder.typicode.com/users';
 // GET
 function sendREquest (method, url)  {
@@ -613,10 +614,10 @@ const body = {
 senDREquest ('POST', requesURL, body)
 	.then(data => console.log(data))
 	.catch(err => console.log(err)); 
-
+}
 
                  // JSON
-
+let jso = () => {
 var xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function () {
 	if (this.readyState == 4 && this.status == 200) {
@@ -626,3 +627,94 @@ xhr.onreadystatechange = function () {
 };
 xhr.open('GET','../json/data.json', true);
 xhr.send();
+}
+// CLass
+
+class Animal {
+	static type = 'ANIMAL'
+	constructor(options) {
+		this.name = options.name
+		this.age = options.age 
+		this.hasTail = options.hasTail
+	}
+voice(){
+	console.log('I am Animal!')
+}
+}
+
+const animal = new Animal({
+	name : 'Animal',
+	age: 5,
+	hasTail: true
+})
+
+class Cat extends Animal {
+	static type = 'CAT';
+
+	constructor (options) {
+		super(options);
+		this.color = options.color;
+	}
+
+	voice() {
+		super.voice();
+		console.log('i am cat')
+	}
+	get ageInfo () {
+		return this.age * 7
+	}
+	set ageInfo (newAge) {
+		this.age = newAge
+	}
+}
+
+ const cat = new Cat ({
+ 	name: 'Cat',
+ 	age: 7,
+ 	hasTail: true,
+ 	color: 'black'
+ })
+  class Component {
+  	constructor(selector) {
+  		this.$el = document.querySelector(selector)
+  	}
+  	hide () {
+  		this.$el.style.display = 'none';
+  	}
+  	show () {
+  		this.$el.style.display = 'block';
+  	 }
+  }
+  class Box extends Component {
+  	 constructor(options) {
+  	 	super(options.selector)
+
+  	 	this.$el.style.width = this.$el.style.height = options.size + 'px'
+  	 	this.$el.style.background = options.color
+  	 }
+  }
+ 
+ const box1 = new Box ({
+ 	selector: '#box1',
+ 	size: 200,
+ 	color: 'red'
+
+ })
+
+ const box2 = new Box({
+ 	selector: '#box2',
+ 	size: 250,
+ 	color: 'lightpink'
+ })
+  class Circle extends Box {
+  	constructor (options) {
+  		super(options)
+
+  		this.$el.style.borderRadius = '50%'
+  	}
+  }
+   const c = new Circle ({
+   	selector: '#circle',
+ 	size: 50,
+ 	color: 'lightgreen'
+   })
